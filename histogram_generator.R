a2509c9f793b6133acde897c8d95cbc2d95bbedb
StephanieDeGraaf <sdegraaf@iastate.edu>
2016-04-23 15:09:53 -0700

    updates, can now overlay two distributions

--- histogram_generator.R
+++ histogram_generator.R
@@ -1,7 +1,12 @@
+library(ggplot2)
+library(reshape2)
+library(plyr)
+
 #x is the wage at each percentile
 x<- c(9.07, 11.27, 17.40, 28.32, 44.29)
+y<- c(8.33,  9.73,	14.61,	22.59, 33.23)
 
-histgen<- function(x){  
+distribution_generator<- function(x){  
   cum.p <- c(.1, .25, .5, .75, .9)
   prob <- c( cum.p[1], diff(cum.p), .1)
   freq <- 10000 
@@ -13,9 +18,16 @@ histgen<- function(x){
   })
   # sample from s, total of freq values, with probabilities from prob 
   out <- sample(s, freq, prob=rep(prob, each=len), replace = T)
-  
-  histogram<- qplot(out, geom="histogram", xlim = c(0, 3*max(out)/4), 
-                    fill = I("blue"), binwidth = 0.75,
-                    main = "Distribution of Wages",  xlab = "Hourly Wage", ylab = "Frequency")
-  return(histogram)  
+  return(out)  
 }
+
+dist_x<- distribution_generator(x)
+dist_y<- distribution_generator(y)
+dat<- melt(data.frame(dist_x, dist_y))
+
+mean_dat <- ddply(dat, "variable", summarise, wage.mean=mean(value))
+
+ggplot(dat, aes(x = value, fill = variable)) + 
+  geom_histogram(xlim = c(0, 3*max(max(dist_x), max(dist_y))/4), binwidth = 0.75, alpha = 0.5)+
+  labs(title = "Distribtuion of Wages", x = "Wage", y= "Frequency")+
+  geom_vline(data=mean_dat, aes(xintercept=wage.mean,  colour=variable))
